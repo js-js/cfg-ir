29cf73635e0f6e6195a445bef581fb95ef9bbcd2
Fedor Indutny <fedor@indutny.com>
2014-10-17 23:32:13 +0400

    lib: turn off greedy matching

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ exports.parse = function parse(source) {
 
     // Instruction
     match = line.match(
-      /^\s*(?:(@)?([\w\d\/\-\.]+)\s*=\s*)?([\w\d\/\-\.]+)(?:\s+([^#]+))?(?:\s*#\s*([\w\d\/\-\.]+))?\s*$/
+      /^\s*(?:(@)?([\w\d\/\-\.]+)\s*=\s*)?([\w\d\/\-\.]+)(?:\s+([^#]+?))?(?:\s*#\s*([\w\d\/\-\.]+))?\s*$/
     );
     if (match === null)
       return;
